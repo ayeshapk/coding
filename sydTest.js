938419b10520267965e529c7c35049d28c43093f
sarun <you@example.com>
2018-01-19 11:33:27 +0700

    add more

--- sydTest.js
+++ sydTest.js
@@ -3,3 +3,8 @@ console.log('second test file')
 console.log('sydney new changes')
 
 console.log('i add change')
+console.log('i add change')
+console.log('i add change')
+console.log('i add change')
+console.log('i add change')
+console.log('i add change')
--- testname.js
+++ testname.js
@@ -3,3 +3,7 @@ var y = 3
 var name = "sarun"
 console.log(name)
 console.log(x+y+x+y)
+console.log('i add change')
+console.log('i add change')
+console.log('i add change')
+console.log('i add change')
