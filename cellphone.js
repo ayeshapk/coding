c979f5f08aacfba91cc0d2462a66a73456243298
sarun <you@example.com>
2018-01-19 16:10:20 +0700

    change phone No

--- cellphone.js
+++ cellphone.js
@@ -1,8 +1,9 @@
 
 var name = "cellphone"
+var phone = "6689654321"
 console.log(name)
 console.log(name)
 console.log('cellphone')
 console.log('number')
 console.log('is')
-console.log('xxx-xxx-xxxx')
+console.log(phone)
