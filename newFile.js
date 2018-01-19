7a60131880991b88b84651f85231b827b69bfcd5
sarun <you@example.com>
2018-01-19 13:22:32 +0700

    test to ignore

--- newFile.js
+++ newFile.js
@@ -3,7 +3,4 @@ var y = 3
 var name = "sarun"
 console.log(name)
 console.log(x+y+x+y)
-console.log('i add change')
-console.log('i add change')
-console.log('i add change')
-console.log('i add change')
+
