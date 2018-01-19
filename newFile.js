aeac46a8b54d8c1037e09ed754c9c7e74eaaa10f
sarun <you@example.com>
2018-01-19 13:17:18 +0700

    test to ignore

---
+++ newFile.js
@@ -0,0 +1,9 @@
+var x = 2
+var y = 3
+var name = "sarun"
+console.log(name)
+console.log(x+y+x+y)
+console.log('i add change')
+console.log('i add change')
+console.log('i add change')
+console.log('i add change')
--- testname.js
+++ testname.js
@@ -1,9 +1,3 @@
-var x = 2
-var y = 3
+
 var name = "sarun"
 console.log(name)
-console.log(x+y+x+y)
-console.log('i add change')
-console.log('i add change')
-console.log('i add change')
-console.log('i add change')
